0d5fc902cac14796c608316de8cd77229ed532f8
Jacky Tang <jickay@gmail.com>
2019-03-10 22:21:22 -0600

    Viz layout structure.

--- visualization.js
+++ visualization.js
@@ -1,15 +1,14 @@
 let selectedStates = 2;
 
-var drawCircles = function (domSelector) {
-    // var json = {
-    //   data: [
-    //     { radius: 15, color: 'red' },
-    //     { radius: 25, color: 'blue' },
-    //     { radius: 5, color: 'yellow' }
-    //   ]
-    // };
+let ageScale = d3.scaleLinear().range([1000, 200]);
 
-    // Notes,State,StateCode,TenYearAgeGroups,TenYearAgeGroupsCode,Gender,GenderCode,Race,RaceCode,Deaths,Population,CrudeRate
+// gridlines in y axis function
+function make_y_gridlines() {		
+  return d3.axisLeft(ageScale)
+} 
+
+var drawCircles = function (domSelector) {
+
 
     // Import dataset
     d3.csv("./data.txt", d => {
@@ -26,10 +25,19 @@ var drawCircles = function (domSelector) {
       console.log("All data:");
       console.log(data);
 
+      ageScale.domain(d3.extent(data, function(d) { return d.age; }));
+
       var svg = d3.select(domSelector)
         .append('svg')
-          .style('width','2000px')
-          .style('height','1200px');
+          .style('width','1500px')
+          .style('height','1100px');
+      
+      svg.append("g")			
+          .attr("class", "grid")
+          .call(make_y_gridlines()
+            .tickSize(-1500)
+            .tickFormat("")
+          );          
 
       svg.selectAll('circle')
         .data(data.filter( d => {
@@ -39,22 +47,23 @@ var drawCircles = function (domSelector) {
           .append('circle')
             .attr('r', function (d) {
               let radius = d.rate;
-              if (radius < 10) { radius = 10; }
-              if (radius !== radius) { radius = 10; }
+              if (radius < 3) { radius = 3; }
+              if (radius !== radius) { radius = 3; }
               return radius;
             })
             .attr('cy', function (d, i) {
               let base = 100,
                   margin = 100;
-              return (d.age * base) + margin;
+                  bottom = 900;
+              return (bottom - d.age * base) + margin;
             })
             .attr('cx', function (d, i) {
               let base = 200,
-                  spacing = 150,
+                  spacing = 170,
                   raceNum = getCX(d.race);
-              var margin = 200,
+              let margin = 200,
                   cx = raceNum * base + (spacing * raceNum),
-                  shift = 100;
+                  shift = 80;
               if (d.gender == "M") {
                 shift = shift;
               } else if (d.gender == "F") {
@@ -72,6 +81,7 @@ var drawCircles = function (domSelector) {
   
     
 };
+
 
 function convertRaceCodes (code) {
   switch(code) {
@@ -116,3 +126,9 @@ function getRaceColor (race) {
     case "native": return "purple";
   }
 }
+
+// Interactive events
+// document.getElementsByClass('.state-btn')[0].addEventListener('click', function() {
+//   console.log(this.id);
+//   updateStates(this.id);
+// })
